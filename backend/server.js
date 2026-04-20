b8f62c7e583b8c1a395e0ff4eb56c65d24f844f8
Neha Shoaib <thisisnehashoaib@gmail.com>
2026-04-20 23:43:03 +0500

    Added JWT authentication and protected routes

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/notes", require("./routes/noteRoutes"));
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res) => {
   res.send("API Running...");
