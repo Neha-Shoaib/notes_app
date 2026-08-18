f55aa5ac0fd871e36aed81e7b9176e1ad2c45e5a
Neha Shoaib <thisisnehashoaib@gmail.com>
2026-08-19 01:36:52 +0500

    fixed padding on top of home screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -96,7 +96,7 @@ const LandingPage = () => {
       </header>
 
       {/* 2. HERO SECTION */}
-      <section className="relative overflow-hidden pt-15 pb-20 md:pt-20">
+      <section className="relative overflow-hidden pt-5  pb-20 md:pt-12">
         {/* Subtle background glow graphics */}
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-7xl h-96 bg-indigo-600/10 blur-[120px] rounded-full pointer-events-none -z-10 dark:bg-indigo-900/20" />
         
